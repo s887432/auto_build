2387ec3bb4963613c6eaaad79bb6ccea0fcccb5a
Patrick Lin <patrick.cclin@gmail.com>
2019-04-20 16:15:02 +0800

    Update auto_build.sh

--- auto_build.sh
+++ auto_build.sh
@@ -7,6 +7,18 @@ rfs_resF="rootfs.tar"
 kernel_resF="zImage"
 dtb_resF="at91-sama5d27_som1_ek.dtb"
 itb_resF="sama5d27_som1_ek.itb"
+mnt1="mnt1"
+mnt2="mnt2"
+
+let sd_card_size_MB=500
+let sd_card_size=sd_card_size_MB*1024*1024
+let first_partitionMB=8
+let first_partition=first_partitionMB*1024*1024
+let first_partition_sectors=$first_partition/512
+let first_offset=2048*512
+let second_offset=first_offset+first_partition
+let first_offset_sectors=first_offset/512
+let second_offset_sectors=second_offset/512
 
 COLOR_ERR='\e[41m'
 COLOR_RST='\e[0m'
@@ -22,6 +34,7 @@ build_rootfs=1
 build_ubootenv=1
 create_cd_card=1
 build_app=1
+build_sdcard=1
 
 echo ""
 echo "START $0 @`date +%y%m%d-%T`......"
@@ -36,3 +49,5 @@ source ./build_scripts/build_kernel.sh
 source ./build_scripts/build_ubootenv.sh
 source ./build_scripts/build_rootfs.sh
 source ./build_scripts/build_app.sh
+source ./build_scripts/build_sdcard.sh
+
