0d5816b357a174dc76ba87decfb4b7483f949744
Patrick Lin <patrick.cclin@gmail.com>
2019-04-29 11:10:17 +0800

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+cur_path = $PWD
 sudo apt-get -y install wget git-core unzip make gcc g++ build-essential subversion sed autoconf automake texi2html texinfo coreutils diffstat python-pysqlite2 docbook-utils libsdl1.2-dev libxml-parser-perl libgl1-mesa-dev libglu1-mesa-dev xsltproc desktop-file-utils chrpath groff libtool xterm gawk fop  libncurses5-dev  gcc-multilib  g++-multilib device-tree-compiler u-boot-tools bison flex
 
+cd /tmp
 wget -c https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/arm-linux-gnueabi/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabi.tar.xz
 wget -c https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/arm-linux-gnueabihf/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf.tar.xz
 
@@ -16,3 +18,6 @@ echo "ARCH=arm" >> ~/.bashrc
 echo "PATH=$PATH:/opt/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabi/bin/" >> ~/.bashrc
 echo "PATH=$PATH:/opt/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf/bin/" >> ~/.bashrc
 
+cd $cur_path
+
+echo "Environment setup is finished."
