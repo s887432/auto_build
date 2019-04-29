f2c3877df3ee65f1583c34ad8e57e8572a91782b
Patrick Lin <patrick.cclin@gmail.com>
2019-04-29 11:07:57 +0800

    Create env_setup.sh

---
+++ env_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get -y install wget git-core unzip make gcc g++ build-essential subversion sed autoconf automake texi2html texinfo coreutils diffstat python-pysqlite2 docbook-utils libsdl1.2-dev libxml-parser-perl libgl1-mesa-dev libglu1-mesa-dev xsltproc desktop-file-utils chrpath groff libtool xterm gawk fop  libncurses5-dev  gcc-multilib  g++-multilib device-tree-compiler u-boot-tools bison flex
+
+wget -c https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/arm-linux-gnueabi/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabi.tar.xz
+wget -c https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/arm-linux-gnueabihf/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf.tar.xz
+
+tar xf gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabi.tar.xz
+tar xf gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf.tar.xz
+
+sudo mv gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabi \opt
+sudo mv gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf \opt
+
+// add following to ~/.bashrc
+echo "export CROSS_COMPILE=arm-linux-gnueabi-" >> ~/.bashrc
+echo "ARCH=arm" >> ~/.bashrc
+echo "PATH=$PATH:/opt/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabi/bin/ >> ~/.bashrc
+echo "PATH=$PATH:/opt/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf/bin/ >> ~/.bashrc
+
