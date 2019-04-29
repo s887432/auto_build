24d62138916c00b1f892cda4bd6914963503d501
user <patrick.cclin@gmail.com>
2019-04-29 11:23:23 +0800

    add test

--- env_setup.sh
+++ env_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Starting to setup building environmet for Linux4Sam..."
+echo "Starting to setup building environmet for Linux4Sam with SAMA5D27_SOM1_EK..."
 cur_path = $PWD
 sudo apt-get -y install wget git-core unzip make gcc g++ build-essential subversion sed autoconf automake texi2html texinfo coreutils diffstat python-pysqlite2 docbook-utils libsdl1.2-dev libxml-parser-perl libgl1-mesa-dev libglu1-mesa-dev xsltproc desktop-file-utils chrpath groff libtool xterm gawk fop  libncurses5-dev  gcc-multilib  g++-multilib device-tree-compiler u-boot-tools bison flex
 
