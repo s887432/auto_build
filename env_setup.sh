bda250777d133321d449052beb42030dca43c78a
user <patrick.cclin@gmail.com>
2019-05-07 05:33:15 +0800

    modify

--- env_setup.sh
+++ env_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Starting to setup building environmet for Linux4Sam ..."
 cur_path = $PWD
-sudo apt-get -y install wget git-core unzip make gcc g++ build-essential subversion sed autoconf automake texi2html texinfo coreutils diffstat python-pysqlite2 docbook-utils libsdl1.2-dev libxml-parser-perl libgl1-mesa-dev libglu1-mesa-dev xsltproc desktop-file-utils chrpath groff libtool xterm gawk fop  libncurses5-dev  gcc-multilib  g++-multilib device-tree-compiler u-boot-tools bison flex
+sudo apt-get -y install wget git-core unzip make gcc g++ build-essential subversion sed autoconf automake texi2html texinfo coreutils diffstat python-pysqlite2 docbook-utils libsdl1.2-dev libxml-parser-perl libgl1-mesa-dev libglu1-mesa-dev xsltproc desktop-file-utils chrpath groff libtool xterm gawk fop  libncurses5-dev  gcc-multilib  g++-multilib device-tree-compiler u-boot-tools bison flex mercurial
 
 cd /tmp
 wget -c https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/arm-linux-gnueabi/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabi.tar.xz
