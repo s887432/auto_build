8c60a7e8ed0177f1bbc60c3fffa2910d216310d9
Patrick Lin <patrick.cclin@gmail.com>
2019-09-24 10:46:10 +0800

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -4,7 +4,7 @@ cur_path = $PWD
 sudo apt-get -y install wget git-core unzip make gcc g++ build-essential subversion sed autoconf automake texi2html texinfo \
 coreutils diffstat python-pysqlite2 docbook-utils libsdl1.2-dev libxml-parser-perl libgl1-mesa-dev libglu1-mesa-dev xsltproc \
 desktop-file-utils chrpath groff libtool xterm gawk fop  libncurses5-dev  gcc-multilib  g++-multilib device-tree-compiler \
-u-boot-tools bison flex mercurial net-tools openssh-server libssl-dev net-tools
+u-boot-tools bison flex mercurial net-tools openssh-server libssl-dev net-tools doxygen graphviz
 
 cd /tmp
 wget -c https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/arm-linux-gnueabi/
