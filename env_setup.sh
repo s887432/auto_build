0a39ae53ace0a2f5d6c410fe2e4d569306e9a378
Patrick Lin <patrick.cclin@gmail.com>
2023-02-22 09:02:56 +0800

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -7,6 +7,11 @@ desktop-file-utils chrpath groff libtool xterm gawk fop  libncurses5-dev  gcc-mu
 u-boot-tools bison flex mercurial net-tools openssh-server net-tools doxygen graphviz libssl-dev gtk-doc-tools
 
 sudo apt-get install -y pkg-config libdrm-dev libinput-dev libcairo-dev libjpeg-dev libmagic-dev gettext
+
+// add for yocto
+sudo apt-get install gawk wget git-core git-lfs diffstat unzip texinfo gcc-multilib build-essential chrpath socat \
+cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev \
+pylint3 xterm zstd
     
 cd /tmp
 wget -c https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/arm-linux-gnueabi\
