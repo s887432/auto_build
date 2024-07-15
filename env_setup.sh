ac0d79fc3e264d7fc1c949d95024b33ccdb16f90
Patrick Lin <patrick.cclin@gmail.com>
2024-07-15 10:28:44 +0800

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -6,7 +6,7 @@ coreutils diffstat docbook-utils libsdl1.2-dev libxml-parser-perl libgl1-mesa-de
 desktop-file-utils chrpath groff libtool xterm gawk fop  libncurses5-dev  gcc-multilib  g++-multilib device-tree-compiler \
 u-boot-tools bison flex mercurial net-tools openssh-server net-tools doxygen graphviz libssl-dev gtk-doc-tools libpkgconf-dev \
 cmake librsvg2-dev liblua5.3-dev libcurl4-openssl-dev libxkbcommon-dev xkb-data \
-libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev sudo apt install libplplot-dev plplot-driver-cairo \
+libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libplplot-dev plplot-driver-cairo \
 libasound2-dev libsndfile1-dev
 
 sudo apt-get install -y pkg-config libdrm-dev libinput-dev libcairo-dev libjpeg-dev libmagic-dev gettext
